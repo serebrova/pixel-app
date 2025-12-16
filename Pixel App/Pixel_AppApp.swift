b6010cfad7fa986240789d73642bfcf092d39d33
Leila Serebrova <leilaserebrova@gmail.com>
2025-12-16 12:09:09 +0500

    Initial Commit

---
+++ Pixel App/Pixel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_AppApp.swift
+//  Pixel App
+//
+//  Created by  Leila Serebrova on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
